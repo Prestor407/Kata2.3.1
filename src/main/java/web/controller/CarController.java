e27b2721ccbe192349e4d38dfaee254d2e4fd93e
Prestor407 <mrprestor@mail.ru>
2025-01-29 16:48:21 +0300

    Готово

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,19 +6,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
-import web.service.CarServiceImpl;
+import web.service.CarService;
+
 
 import java.util.List;
 
 @Controller
 public class CarController {
 
-
-    private CarServiceImpl carService;
+    private CarService carService;
 
     @Autowired
-    public CarServiceImpl setCarService(CarServiceImpl carServiceImpl) {
-        return carServiceImpl;
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
 
     @GetMapping("/cars")
